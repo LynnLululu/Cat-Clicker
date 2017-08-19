c191eb2d8a10cedae42ae78e917ccc483cfd03cc
LynnLululu <luyiqun130@foxmail.com>
2017-08-19 21:56:46 +0800

    shit typo

--- main.js
+++ main.js
@@ -4,5 +4,5 @@ var n = 0;
 cat.addEventListener('click', function(){
   //the element has been clicked... do stuff here
   n++;
-  document.getElementById('num').innerHtml = "click count:" + n;
+  document.getElementById('num').innerHTML = "Click Count: " + n;
 }, false);
